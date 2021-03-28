d8c59e027696fe4260e9dafef59c2dfc2fe1bad5
Ivan Miller <i.s.miller@ya.ru>
2021-03-28 12:27:59 +0700

    Метод расширения для JsonElement

---
+++ src/Octoller.CurrencyConverter/Octoller.CurrencyConverter.App/Infrastructure/Extension/JsonDocumentExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Octoller.CurrencyConverter.App.Infrastructure.Extension
+{
+    /// <summary>
+    /// Расширение парсинга для <see cref="JsonElement"/>.
+    /// </summary>
+    public static class JsonDocumentExtensions
+    {
+        /// <summary>
+        /// Получает строковое значение указанного свойства <see cref="JsonElement"/>.
+        /// </summary>
+        /// <param name="element"><see cref="JsonElement"/>.</param>
+        /// <param name="key">Имя свойства.</param>
+        /// <returns>Значение свойства.</returns>
+        public static string GetElement(this JsonElement element, string key)
+        {
+            if (element.TryGetProperty(key, out JsonElement property) && property.ValueKind != JsonValueKind.Null)
+            {
+                return property.ToString();
+            }
+
+            return string.Empty;
+        }
+    }
+}
