10effeaf252822f40855ff7a4806c4b2805ca84e
Ivan Miller <i.s.miller@ya.ru>
2021-03-28 20:13:03 +0700

    Шаблонные команды

---
+++ src/Octoller.CurrencyConverter/Octoller.CurrencyConverter.App/Infrastructure/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Octoller.CurrencyConverter.App.Infrastructure.Commands
+{
+    public abstract class Command : ICommand
+    {
+        ///<inheritdoc />
+        public event EventHandler CanExecuteChanged;
+
+        ///<inheritdoc />
+        public abstract bool CanExecute(object parameter);
+
+        ///<inheritdoc />
+        public abstract void Execute(object parameter);
+    }
+}
---
+++ src/Octoller.CurrencyConverter/Octoller.CurrencyConverter.App/Infrastructure/Commands/TemplateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Octoller.CurrencyConverter.App.Infrastructure.Commands
+{
+    public class TemplateCommand : Command
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        ///<inheritdoc />
+        public TemplateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        ///<inheritdoc />
+        public override bool CanExecute(object parameter) =>
+            canExecute?.Invoke(parameter) ?? true;
+
+        ///<inheritdoc />
+        public override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            execute?.Invoke(parameter);
+        }
+    }
+}
